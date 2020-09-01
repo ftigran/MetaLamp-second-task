89eb85cedadaa6bf83f46ec7e614e751d6a736db
Tigran Frangyan <ftigran1@gmail.com>
2020-09-01 16:35:27 +0300

    добавил expandable checkbox list

---
+++ src/modules/dropdown/dropdown.js
@@ -0,0 +1,12 @@
+var dropdownRoom=document.getElementsByClassName('dropdown dropdown_expanded dropdown_for-room')
+
+document.addEventListener('click', function(e){
+    if (e.target.className=="dropdown_for-room")
+    console.log('dsadsa')
+})
+dropdownRoom.onClick = function() {
+console.log(dropdownRoom)
+console.log('dsssssssssssdadsa')
+
+}
+console.log('228')
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,1 +1,5 @@
 import './form-elements.scss'
+
+import {
+    dropdown
+} from '../../modules/dropdown/dropdown.js'
