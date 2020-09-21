7fbb4fba7d0668f2ae4b856106a9a50d9c50dd93
Tigran Frangyan <ftigran1@gmail.com>
2020-09-22 00:37:08 +0300

    Сделал частично js для dropdown

--- src/modules/dropdown/dropdown.js
+++ src/modules/dropdown/dropdown.js
@@ -1,25 +1,66 @@
 import './dropdown.scss';
 
-let maxVal=7;
+let maxVal=6;
 let minVal=0;
 let droptownRoomsText=[];
+let droptownRoomsDefaultText='Удобства номера'
+let totalGuests=0;
+let guests= [{
+  item:'guest',
+  words:['Сколько гостей','гость','гостя','гостей'],
+}];
 let itemsRooms = [
       {
       item:'bedroom',
-      words:['спальня','спальни','спален'],
+      words:['','спальня','спальни','спален'],
       },
       {
         item:'bed',
-        words:['кровать','кровати','кроватей'],
+        words:['','кровать','кровати','кроватей'],
       }
       ,
       {
         item:'bathroom',
-        words:['ванная комната','ванные комнаты','ванных комнат'],
-      }
+        words:['','ванная комната','ванные комнаты','ванных комнат'],
+      },
       ]
 
+let type;
+let item;
+let val;
+let DropdownText;
 
+//Задаёт текст в дропдауне, согласно его типу и значениям его параметров
+function dropdownInitialize(dropdown){
+  type = $(dropdown).find('.dropdown__item').data('type');
+  DropdownText=$(dropdown).find('.dropdown__text');
+
+  if  (type=='room'){
+    $(dropdown).find('.dropdown__item').each(function(i,elem){
+      item = $(this).data('item');
+      val=$(this).find('.dropdown__quantity').text();
+    dropdownTextSemantica(val, itemsRooms, droptownRoomsText);
+    calcDropdownText(DropdownText)
+    })
+  } else if (type=='guest'){
+    item = $(dropdown).find('.dropdown__item').data('item');
+    $(dropdown).find('.dropdown__quantity').each(function(i,elem){
+      totalGuests+=+$(this).text();
+    })
+
+    let arr=[];
+
+    dropdownTextSemantica(totalGuests, guests, arr);
+    setDropdownText(DropdownText, arr[0])
+  }
+}
+
+//Присвоение значений текста в дропдаунах прии загрузке страницы
+$('.dropdown').each(function(i,elem){
+  dropdownInitialize(this)
+});
+
+//обработка клика по дропдауну
 $('.dropdown__text').click(function (event) {
   const dropdown=$(this).parent('.dropdown');
 
@@ -27,16 +68,19 @@ $('.dropdown__text').click(function (event) {
   $(dropdown).find('.dropdown__menu').toggleClass('dropdown__menu_showed');
 });
 
+//обработка клика по кнопкам плюс и минус
 $('.dropdown__button').click(function (event) {
 
   const parent = $(this).parent('.dropdown__controls');
   const value = $(parent).find('.dropdown__quantity');
-  const item = $(parent).parent('.dropdown__item').data('item')
-  let val = $(value).text();
-//const text= 
-//const text = $(parent).parent('.dropdown__item').parent('.dropdown__menu').parent('.dropdown').find('.dropdown__text').text(message);
+  item = $(parent).parent('.dropdown__item').data('item');
+  type = $(parent).parent('.dropdown__item').data('type')
+  val = $(value).text();
+  let isCalc=false;
+  
   if (this.textContent=='+'){
     val++;
+    isCalc=true;
     if (val==maxVal) {
       $(this).attr('disabled', 'true')
     } else if(val==minVal+1) {
@@ -51,41 +95,81 @@ $('.dropdown__button').click(function (event) {
     }
   }
 
-  $(value).text(val);//присвоение значения
+  $(value).text(val);//присвоение значения параметру
+  
+  DropdownText= $(parent).parent('.dropdown__item').parent('.dropdown__menu').parent('.dropdown').find('.dropdown__text');
 
-  dropdownTextSemantica(val, item);
-
-  const DropdownText= $(parent).parent('.dropdown__item').parent('.dropdown__menu').parent('.dropdown').find('.dropdown__text');
-  setDropdownText(DropdownText)
-
+  dropdownTypeDistribution(DropdownText,isCalc);
 });
-function setDropdownText(elem){
-  let text=''; let n = 0;
-  for (let droptownRoomText of droptownRoomsText) {
-    text+=droptownRoomText;
-    if (n!=droptownRoomsText.length-1){
-      text+=', ';
-      n++;
-    };
+
+//
+function dropdownTypeDistribution(text,isCalc){
+  if  (type=='room'){
+    dropdownTextSemantica(val, itemsRooms, droptownRoomsText);
+    calcDropdownText(text)
+  } else if (type=='guest'){
+    isCalc ? totalGuests++:totalGuests--;
+    let arr=[];
+    dropdownTextSemantica(totalGuests, guests, arr);
+    setDropdownText(text, arr)
   }
+}
+
+//задать текст дропдауну
+function setDropdownText(elem, text){
   $(elem).text(text);
 }
-function dropdownTextSemantica(val, item){
+
+//сбор слов с разных параметров о один текст
+function calcDropdownText (elem){
+  let text=''; 
+  for (let droptownRoomText of droptownRoomsText) {
+    if (droptownRoomText!='' && droptownRoomText!=droptownRoomsText[0]){
+      text+=', ';
+    };
+    text+=droptownRoomText;
+  }
+  if (text=='') text=droptownRoomsDefaultText;
+  setDropdownText(elem, text)
+}
+
+//Определение необходимого окончания
+function dropdownTextSemantica(val, array, outputArray){
   let n=0;
-  for (let itemsRoom of itemsRooms) {
-    if(itemsRoom.item==item){
-      
-      let message = val+' ';
-      if (val==1){
-        message+=itemsRoom.words[0];
-      } else if (val<=4&&val>1){
-        message+=itemsRoom.words[1];
+  for (let arr of array) {
+    if(arr.item==item){
+      let message;
+      if (val==0){
+        message=arr.words[0];
+      } else if (val==1){
+        message= val+' '+arr.words[1];
+      } else if (val<=4){
+        message=val+' '+arr.words[2];
       }else {
-        message+=itemsRoom.words[2];
+        message=val+' '+arr.words[3];
       };
-      droptownRoomsText[n]= message;
+      outputArray[n]= message;
       break;
     }
 n++;
   }
 }
+
+//Клик по кнопке "Применить"
+$('.button_purple').click(function (event) {
+  const dropdown=$(this).parent('.dropdown__controls').parent('.dropdown__menu').parent('.dropdown')
+  
+  $(dropdown).find('.dropdown__menu').toggleClass('dropdown__menu_showed');
+  $(dropdown).toggleClass('dropdown_expanded');
+}) 
+
+//клик по кнопке "Очистить"
+$('.button_gray').click(function (event) {
+  const dropdown=$(this).parent('.dropdown__controls').parent('.dropdown__menu').parent('.dropdown')
+  const dropdownItem = $(dropdown).find('.dropdown__item').each(function(){
+    let isGuest = $(this).data('type')=='guest';
+    if (isGuest)  totalGuests=0;
+    $(this).find('.dropdown__quantity').text(minVal);
+  })
+  dropdownInitialize(dropdown);
+}) 
