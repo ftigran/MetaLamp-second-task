d232a77b80d5b58f8522de0e37caf3a42628b9a9
Tigran Frangyan <ftigran1@gmail.com>
2020-09-22 04:10:05 +0300

    Добавил js, анимацию expandable-checkbox-list

---
+++ src/modules/expandable-checkbox-list/expandable-checkbox-list.js
@@ -0,0 +1,9 @@
+import './expandable-checkbox-list.scss'
+
+$('.expandable-checkbox-list__title').click(function (event){
+    const parent = $(this).parent('.expandable-checkbox-list');
+    $(this).toggleClass('expandable-checkbox-list__title_expanded')
+    console.log($(this).text())
+    //$(parent).toggleClass('.expandable-checkbox-list_expanded')
+    $(parent).find('.expandable-checkbox-list__list').toggleClass('expandable-checkbox-list__list_show')
+})
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,5 +3,6 @@ import './form-elements.scss'
 import * as $ from 'jquery'
 import'../../modules/range-slider/range-slider'
 import '../../modules/dropdown/dropdown'
+import '../../modules/expandable-checkbox-list/expandable-checkbox-list'
 //import'../../modules/like-button/like-button'
 
