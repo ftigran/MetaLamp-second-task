ce0a8d8dc6bf4f320b9e27ed403fc7d0bc87e8a5
Tigran Frangyan <ftigran1@gmail.com>
2020-09-23 21:35:04 +0300

    добавил masked-text-field

---
+++ src/modules/masked-text-field/masked-text-field.js
@@ -0,0 +1,9 @@
+import './masked-text-field.scss'
+
+import 'cleave.js'
+
+var cleave = new Cleave('.masked-text-field__input', {
+    date: true,
+    delimiter: '.',
+    datePattern: ['d', 'm', 'Y']
+});
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -7,5 +7,5 @@ import '../../modules/expandable-checkbox-list/expandable-checkbox-list'
 import '../../modules/like-button/like-button'
 import '../../modules/date-dropdown/date-dropdown'
 import '../../modules/filter-date-dropdown/filter-date-dropdown'
-
+import '../../modules/masked-text-field/masked-text-field'
 
