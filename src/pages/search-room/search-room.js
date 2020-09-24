77bd3cf6796a01b13d75eeb68b3484e54994f6cb
Tigran Frangyan <ftigran1@gmail.com>
2020-09-24 03:17:42 +0300

    Частично сверстал search-room страницу

---
+++ src/pages/search-room/search-room.js
@@ -0,0 +1,8 @@
+import './search-room.scss'
+import '../../modules/header/header'
+import '../../modules/dropdown/dropdown'
+import '../../modules/filter-date-dropdown/filter-date-dropdown'
+import '../../modules/range-slider/range-slider'
+import '../../modules/expandable-checkbox-list/expandable-checkbox-list'
+import '../../modules/room-card/room-card'
+import '../../modules/footer/footer'
