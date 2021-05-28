644c66d05c2a5752fdd87a3e69fdd027b4ed4e31
Тигран Франгян <frangyan@atoms.ru>
2021-05-28 13:11:22 +0300

    fix typos

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,8 @@
 import "./index.scss";
+window.onload = function () {
+  const e = document.getElementById("preloader");
+  e.classList.add("anim_fade_out"),
+    setTimeout(function () {
+      e.remove();
+    }, 300);
+};
